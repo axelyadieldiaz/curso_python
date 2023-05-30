66e094e1aa7c0d4b7fc534f59a0b95beb380c320
axelyadieldiaz <alexddiax@gmail.com>
2023-05-30 10:17:30 -0500

    actualizacion de mi carpeta curso python 2

--- converciones/ejercicios.py
+++ converciones/ejercicios.py
@@ -15,7 +15,6 @@ print (f"""
 =================
 """)
 
-
 #hacer un programa que pida a un usuario un texto y me de
 #como salida la cantidad de letras que tiene
 ##entrada=hola
@@ -36,3 +35,38 @@ if edad<18:
 		print('ya come')
 else:
 	print('ya esta usado')
+##hacer un programa que pida al usuario su DNI si la
+##longitud del DNI es 8 pida que su nombre y lo muestre 
+##por consola si la longitud del DNI es mayor o menor que 
+##a 8 que le muestre mensaje de error
+
+dni = input("Ingrese su DNI: ")
+
+if len(dni) == 8:
+    nombre = input("Ingrese su nombre: ")
+    print("Su nombre es:", nombre)
+else:
+    print("Error: La longitud del DNI debe ser 8.")
+
+apellido = input("Ingrese su primer apellido: ")
+## hacer un programa que pida al usuario ingresar su primer apellidos si su apellido
+##tiene como ultimos caracteres las letras --ez-- mostar un mensaje que digas eres
+##casi español si los caracteres finales son --es-- que digas eres casi peruano
+
+if apellido[-2:] == "ez":
+    print("Eres casi español.")
+elif apellido[-2:] == "es":
+    print("Eres casi peruano.")
+else:
+    print("No se puede determinar tu nacionalidad.")
+
+##hacer un programa que le pida a un usuario di DNI compruebe qeu sea de 8 digitos
+##si es correcto que sume el primer numero y el ultimo numero del DNI, mostar 
+##por la pantalla la suma y el resultado
+dni = input("ingrese su DNI")
+suma = len [0]+[-1]
+if len(dni) == 8:
+    dni =print("suma del primer y el ultimo digito de tu DNI")
+    print("la suma es")
+else:
+      print("error: la longitud de tu DNI debe ser 8.")
