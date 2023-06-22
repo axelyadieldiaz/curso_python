f01cb07bb4ef2cf519470e8fb5c46c53b0c357d5
axelyadieldiaz <alexddiax@gmail.com>
2023-06-22 12:28:23 -0500

    clase funcion for

--- controles_de_flujo/main.py
+++ controles_de_flujo/main.py
@@ -1,24 +1,45 @@
 ## bucles
-condicion=0
-while condicion<11:
-    print("condicion")
-    condicion=condicion+1
+##condicion=0
+##while condicion<11:
+##    print("condicion")
+##    condicion=condicion+1
 
-hola=0
-while hola<17:
-    print("hola")
-    hola=hola+1
+##hola=0
+##while hola<17:
+##    print("hola")
+##    hola=hola+1
 
-edad=0
-while edad != 20:
-    edad=int(input("ingrese su edad: "))
+##edad=0
+##while edad != 20:
+##    edad=int(input("ingrese su edad: "))
 
-nombre=""
-while nombre != "si":
-    nombre=input("ingresa tu nombre: ")
+##nombre=""
+##while nombre != "si":
+##    nombre=input("ingresa tu nombre: ")
 
-while True:
-    nombre=input('como te llamas:')
-    if nombre == 'si':
+##while True:
+##    nombre=input('como te llamas:')
+##    if nombre == 'si':
+##        break
+
+##for numero in range(1,20):
+##    print(numero)
+
+##vocales=['a','e','i','o','u']
+##for numero in range(0,5):
+##    print(vocales[numero])
+
+##for voca in vocales:
+##    print(vocales)
+
+## crear una lista de cinco colores cada color que interes
+## tendras que mostrar por consola, solo cuando encuentre el
+## color rojo mostrara el mensaje de 
+## color encontrado y se terminara la ejecucion
+
+colores = ['azul','naranja','negro','rojo','blanco']
+for color in colores:
+    if color == "rojo":
+        print("color encontrado.")
         break
-
+    print(color)
